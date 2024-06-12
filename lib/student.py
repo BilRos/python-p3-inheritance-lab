1cdc1a9bfef8f437adf29eaef7b37a636b0ba0d7
BilRos <billyrossowuor@gmail.com>
2024-06-12 09:50:32 +0300

    object inheritance lab 1

--- lib/student.py
+++ lib/student.py
@@ -2,7 +2,23 @@
 
 from user import User
 
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
     
-    def learn(self):
-        pass
+    def learn(self,knowledge):
+        self.knowledge.append(knowledge)
+    
+
+        
--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,21 @@ import random
 
 class Teacher(User):
 
+    def __init__(self, last_name, first_name):
+        super().__init__(last_name, first_name)
+        self.knowledge =  [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+
     def teach(self):
         pass
+        new_knowledge = random.randint(0, len(self.knowledge)-1)   
+        return self.knowledge[new_knowledge]
+        
